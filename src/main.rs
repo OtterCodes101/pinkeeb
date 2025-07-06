f320b5f7b85757277ba6fc337cce7ea3a430f8e7
Leah Anderson <leah@chromebooks.lol>
2025-07-06 15:06:08 -0600

    meoowwww :3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use std::f32::consts::FRAC_PI_2;
-
 use asteroids::{
-	elements::{shape, Dial, GrabRing, Lines}, // Brings in elements used in the client
-	ClientState,
-	ElementTrait,
-	Migrate,
-	Transformable,
+	elements::{Grabbable, Model},
+	ClientState, CustomElement, Migrate,
 };
 use glam::{Quat, Vec3};
 use serde::{Deserialize, Serialize};
-use stardust_xr_fusion::{fields::Shape, project_local_resources};
+use stardust_xr_fusion::{
+	fields::{CylinderShape, Shape},
+	project_local_resources,
+};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
@@ -18,18 +16,16 @@ async fn main() {
 
 #[derive(Debug, Serialize, Deserialize)] // Defining variables used in client
 pub struct State {
-	grab_pos: Vec3,
-	time: f32,
-	cube_edge_length: f32,
+	pos: Vec3,
+	rot: Quat,
 }
 
 impl Default for State {
 	// Defines the default value for the variables used in the client
 	fn default() -> Self {
 		Self {
-			grab_pos: Default::default(),
-			time: Default::default(),
-			cube_edge_length: 0.1,
+			pos: Vec3::ZERO,
+			rot: Quat::IDENTITY,
 		}
 	}
 }
@@ -37,33 +33,24 @@ impl Migrate for State {
 	type Old = Self;
 }
 impl ClientState for State {
-	const APP_ID: &'static str = "org.example.client_template";
+	const APP_ID: &'static str = "lol.chromebooks.leah.pinkeeb";
 
 	// Example: Cube with grab ring on the bottom (allows user to move it)
 	// and a dial on the top that allows the user to change the size of the box
 	fn reify(&self) -> asteroids::Element<Self> {
-		let dial = Dial::create(self.cube_edge_length, |state: &mut Self, value: f32| {
-			state.cube_edge_length = value.clamp(0.05, 0.9); // Restrict size between 0.05 &
-			                                        // 0.9 meters
-		})
-		.turn_unit_amount(0.2) // Defines how much 1 rotation will add or subtract from cube_edge_length
-		.radius(0.05) // Defines the radius of the dial
-		.thickness(0.04) // Defines the thickness of the dial
-		.pos([0.0, self.cube_edge_length / 2.0, 0.0]) // Sets position of rotation of dial (on to of the cube)
-		.rot(Quat::from_rotation_x(-FRAC_PI_2)) // Sets orientation to correct plane
-		.build(); // Builds the object
-		let lines = Lines::new(shape(Shape::Box([self.cube_edge_length; 3].into()))) // Creates box outline size
-			.pos([0.0, self.cube_edge_length / 2.0, 0.0]) // Updates position of box
-			.with_children([dial]);
-		GrabRing::new(self.grab_pos, |state: &mut Self, pos| {
-			// Creates Grabbable ring underneath box
-			state.grab_pos = pos.into();
-		})
-		.radius(self.cube_edge_length.hypot(self.cube_edge_length) / 2.0 + 0.01) // Defines radius of grab ring
-		.with_children([lines]) // Makes the box become a child of the ring
+		Grabbable::new(
+			Shape::Cylinder(CylinderShape {
+				length: 0.03,
+				radius: 0.01,
+			}),
+			self.pos,
+			self.rot,
+			|state: &mut Self, pos, rot| {
+				state.pos = pos.into();
+				state.rot = rot.into();
+			},
+		)
+		.build()
+		.child(Model::namespaced("pinkeeb", "pin").build())
 	}
-
-	fn on_frame(&mut self, _info: &stardust_xr_fusion::root::FrameInfo) {
-		self.time += _info.delta;
-	} // scale before identical scale to when it reloads
 }
